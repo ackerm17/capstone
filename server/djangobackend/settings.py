ed8468ccc3cf6ddcd87d264383ca774b6e8b4a7b
ackerm17 <ackerm17@yahoo.com>
2023-12-23 11:09:18 -0500

    halfway through 2

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -22,6 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'ao5z(o(z@cvzodm99d32jkxa5e8a1!q_4sqss5-a%n6tg$#h$+'
 
+CSRF_TRUSTED_ORIGINS=['https://ackerm17-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
--- server/djangobackend/urls.py
+++ server/djangobackend/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('admin/', admin.site.urls),
